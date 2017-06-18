790d25953e96ef98f7e08153a52c93be2968d16c
Марина Звягина <zvyagina.marina@gmail.com>
2017-06-18 21:46:38 +0300

    Initial Commit

---
+++ FlickrDownloadAppWithTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrDownloadAppWithTable
+//
+//  Created by Марина Звягина on 18.06.17.
+//  Copyright © 2017 Zvyagina Marina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrDownloadAppWithTable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrDownloadAppWithTable
+//
+//  Created by Марина Звягина on 18.06.17.
+//  Copyright © 2017 Zvyagina Marina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
